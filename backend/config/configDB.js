d313b47f86cd13f2d9d0cf27b4aa53f4626b8b7b
Sanjid Rahman <rahmansanjid505@gmail.com>
2024-02-15 23:16:14 +0530

    created DB connection setup

---
+++ backend/config/configDB.js
@@ -0,0 +1,9 @@
+const dbConfig = {
+    HOST: "localhost",
+    USER: "sanjid",
+    PASSWORD: "sanjid123",
+    DATABASE: "user_management",
+    DIALECT: "mysql",
+};
+
+module.exports = dbConfig;
