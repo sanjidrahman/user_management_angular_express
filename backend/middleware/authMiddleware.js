93c8b75ac962c8e64b51d4d3c0fc0a386e3f001f
Sanjid Rahman <rahmansanjid505@gmail.com>
2024-02-16 15:17:48 +0530

    Added middleware for user authentication and error handling

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const db = require('../models/index.js')
+const User = db.models
+const jwt = require('jsonwebtoken')
+
+const auth = async (req, res, next) => {
+    try {
+        const authHeaders = req.headers.authorization;
+        if (authHeaders && authHeaders.split(' ')[1]) {
+            const token = authHeaders.split(' ')[1];
+            if (!token) {
+                res.status(401).json({ message: 'Not Authorization' })
+            }
+
+            const decoded = jwt.verify(token, process.env.JWTSECRET);
+            req.body.userId = decoded.payload.id;
+
+            next();
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'Internal Server Error' })
+    }
+}
+
+module.exports = {
+    auth
+}
